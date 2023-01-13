cde33d52d4d913ed42246797f75cc0246d545ffb
Geraldin Kaba <arnaudgeraldinkaba@gmail.com>
2023-01-13 03:35:48 +0100

    Application Java 1.0

---
+++ src/classes/Connexion.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import java.sql.*;
+
+/**
+ *
+ * @author SystellCOM
+ */
+public class Connexion {
+     //chemin de pilote
+    String urlpilote="com.mysql.jdbc.Driver";
+    String urlbaseDonnes="jdbc:mysql://localhost:3306/gestiondr1";
+    Connection conn;
+    
+    public  Connexion(){
+        try {
+            Class.forName(urlpilote);
+            System.out.println("chargemment de pilote reussi");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e);
+        }
+        try {
+           conn= DriverManager.getConnection(urlbaseDonnes, "root", "");
+            System.out.println("chargemment avec la base de données reussi");
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+    public Connection ObtenirConnexion(){
+    return conn;
+    }
+}
